1322c581bd8eefdc52611ac6141a8b5aa4219617
AnitaSelorm <aeselorm@st.ug.edu.gh>
2024-05-19 20:50:53 +0000

    Add files via upload
    TASK 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,10 @@
 function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
-} 
+}
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        const num = numbers[index];
+        return num % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
